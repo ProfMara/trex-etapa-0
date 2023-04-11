a2d260def8f8ef05cd7862f63e8934a96d198cc8
ProfMara <94469709+ProfMara@users.noreply.github.com>
2023-04-11 09:14:57 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,20 +1,26 @@
+//é aqui que cria as variáveis
 
 
+//essa função serve para carregar arquivos de mídia
+function preload() { 
 
-function preload() {
 
 }
 
-
+//essa função executa os comandos apenas uma vez antes do jogo começar
 function setup() {
+    //cria a área de jogo
     createCanvas(600, 200);
 
 
+
 }
-
-
+//executa os comandos por todo o jogo
 function draw() {
-    background("grey")
-   
+    //pinta o fundo de branco
+    background("skyblue");
+
+
+    //desenha as sprites
     drawSprites();
 }
