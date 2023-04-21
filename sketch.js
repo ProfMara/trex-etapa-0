e4749a4330b4a0c98adf0d238d3a52a62b76f45e
ProfMara <94469709+ProfMara@users.noreply.github.com>
2023-04-21 16:51:51 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,25 +2,8 @@
 
 
 //essa função serve para carregar arquivos de mídia
-function preload() { 
-
-
-}
 
 //essa função executa os comandos apenas uma vez antes do jogo começar
-function setup() {
-    //cria a área de jogo
-    createCanvas(600, 200);
 
-
-
-}
 //executa os comandos por todo o jogo
-function draw() {
-    //pinta o fundo de branco
-    background("skyblue");
 
-
-    //desenha as sprites
-    drawSprites();
-}
